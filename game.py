483aafced00adae18e71c2a4f1f0eb25dbae28fd
Matthieu Rousseau <matthieurousseau86@gmail.com>
2021-12-16 18:57:08 +0100

    Test Travis

--- game.py
+++ game.py
@@ -13,7 +13,6 @@ class Game():
     def is_valid(self, word):
         if not word:
             return False
-
         grid_copy = self.grid.copy()
         word_letters = list(word)
         for word_letter in word_letters:
